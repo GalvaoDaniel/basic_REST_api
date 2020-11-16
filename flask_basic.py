f1a0397a9c28d7e9c405f303d2a04802f501acaa
Daniel Galvão <galvaoazevedod@gmail.com>
2020-11-16 00:20:28 -0300

    upgrade - adding Flask_restful module

--- flask_basic.py
+++ flask_basic.py
@@ -1,14 +1,23 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class HelloWorld(Resource):
+    def get(self):
+        return {'about': "Hello World!"}
 
-@app.route('/multi/<int:num>' , methods=['GET'])
-def get_multiply10(num):
-    return jsonify({"result" : num*10})
+    def post(self):
+        some_json = request.get_json()
+        return {'you sent': some_json}, 201
+
+class Multi(Resource):
+    def get(self, num):
+        return {'result': num*10}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Multi, '/multi/<int:num>')
 
 if __name__ == '__main__':
     app.run(debug=True)
